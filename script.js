4801e1ecc2a645e1c88fb1f6212e68fa30408baf
Alex Eesley <alexandra.eesley@gmail.com>
2020-10-07 09:39:28 -0500

    moved js over to js file and make sure link was working

---
+++ script.js
@@ -0,0 +1,37 @@
+function searchWeather(name) {
+    
+    var APIKey = "e79e860f1526eb9cc2572046fff7a30c"; // currently Alex's API key
+    var userInput = $("#city-input").val();
+    // building URL we need to access API
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ userInput+"&appid=" + APIKey;
+
+    $.ajax({ // Ajax function to access query
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+
+        var cityName = $("<div>").text(response.name); // creating the variable for city name
+        var tempK = response.main.temp;
+        var tempC = (tempK - 273.15)*1.80+32;
+        var humidity = response.main.humidity;
+        console.log(humidity);
+        var windSpeed = response.wind.speed;
+
+
+
+        $("#city-name").empty(); // emptying div
+        $("#city-name").append(cityName); // appending the under inputted city name, and the corresponding main weather data value
+        $("#temp").append("Temperature: " + tempC.toFixed(2) + " °F");
+        $("#humidity").append("Humidity: " + humidity + " %");
+        $("#wind-speed").append("Wind Speed: " + windSpeed + " MPH");
+
+    });
+}
+
+$("#select-city").on("click", function(event) { // creating the on click event to take in the user input city value
+    event.preventDefault();
+    var inputCity = $("#city-input").val().trim();
+
+    searchWeather(inputCity);
+});
