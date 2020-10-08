3bf2b0559557dd847210e92d5aa0bb46d4f89166
Alex Eesley <alexandra.eesley@gmail.com>
2020-10-07 21:05:54 -0500

    added first layer of variables and functioning API data pull and added to the HTML elemends for the first of the 5 day forecasts sections. It's working minus the icons

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 function searchWeather(name) {
-    
+
+    // VARIABLES TO USE FOR BOTH QUERY URLS
     var APIKey = "e79e860f1526eb9cc2572046fff7a30c"; // currently Alex's API key
     var userInput = $("#city-input").val();
-    // building URL we need to access API
+    // START OF THE FIRST QUERY URL USED FOR CURRENT WEATHER
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ userInput+"&appid=" + APIKey;
 
     $.ajax({ // Ajax function to access query
         url: queryURL,
         method: "GET"
     }).then(function(response) {
-        console.log(response);
+        // console.log(response);
 
+        // VARIABLES TO CALL & POPULATE CURRENT WEATHER
         var cityName = $("<div>").text(response.name); // creating the variable for city name
         var tempK = response.main.temp;
         var tempC = (tempK - 273.15)*1.80+32;
         var humidity = response.main.humidity;
         var windSpeed = response.wind.speed;
 
-
-
+        // ADDING CURRENT WEATHER DATA TO WEATHER DASHBOARD HTML
         $("#city-name").empty(); // emptying div
         $("#city-name").append(cityName); // appending the under inputted city name, and the corresponding main weather data value
         $("#temp").append("Temperature: " + tempC.toFixed(2) + " °F");
@@ -26,7 +27,7 @@ function searchWeather(name) {
         $("#wind-speed").append("Wind Speed: " + windSpeed + " MPH");
 
     });
-
+    // START OF THE SECOND QUERY URL USED FOR THE 5 DAY FORECAST
     var queryURLForecast = "https://api.openweathermap.org/data/2.5/forecast?q="+userInput+"&appid=" + APIKey;
 
     $.ajax({
@@ -34,9 +35,25 @@ function searchWeather(name) {
         method: "GET"
     }).then(function(response) {
         console.log(response);
-    })
-}
 
+        var forecastDate = JSON.stringify(response.list[0].dt_txt);
+        // var forecastIcon = JSON.stringify(response.list[0].dt_txt);
+        var forecastTempK = response.list[0].main.temp;
+        console.log(forecastTempK);
+        var forecastTempC = (forecastTempK - 273.15)*1.80+32;
+        // var forecastTemp = response.list[0].main.temp;
+        var forecastHum = response.list[0].main.humidity;
+
+        $("#forecast1").append(forecastDate);
+        $("#forecasttemp1").append("Temp: " + forecastTempC.toFixed(2) + " °F");
+        $("#forecasthum1").append("Humidity: " + forecastHum + "%");
+
+
+
+    })
+
+}
+// ON CLICK FUNCTION THAT MAKES THE CURRENT WEATHER POPULATE
 $("#select-city").on("click", function(event) { // creating the on click event to take in the user input city value
     event.preventDefault();
     var inputCity = $("#city-input").val().trim();
